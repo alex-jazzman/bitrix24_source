92da98cc7ccf527a1ed8591fc2959e7d54b968d9
Alex JazzMan <aleksandr.jazzman@gmail.com>
2022-06-22 13:18:32 +0700

    class alias helpers

---
+++ bitrix/aliases.php
@@ -0,0 +1,593 @@
+<?
+
+namespace Bitrix\Bizproc {
+    class WorkflowTemplateTable extends \Bitrix\Bizproc\Workflow\Template\Entity\WorkflowTemplateTable {
+    }
+}
+
+namespace Bitrix\Bizproc {
+    class WorkflowInstanceTable extends \Bitrix\Bizproc\Workflow\Entity\WorkflowInstanceTable {
+    }
+}
+
+namespace Bitrix\Bizproc {
+    class WorkflowStateTable extends \Bitrix\Bizproc\Workflow\Entity\WorkflowStateTable {
+    }
+}
+
+namespace Bitrix\Crm {
+    class CommunicationType extends \Bitrix\Crm\Communication\Type {
+    }
+}
+
+namespace Bitrix\Crm\Order {
+    class OrderBuilderCrm extends \Bitrix\Crm\Order\Builder\OrderBuilderCrm {
+    }
+}
+
+namespace Bitrix\Crm\Order {
+    class OrderBuilderRest extends \Bitrix\Crm\Order\Builder\OrderBuilderRest {
+    }
+}
+
+namespace Bitrix\Crm\Order {
+    class BasketBuilderCrm extends \Bitrix\Crm\Order\Builder\BasketBuilderCrm {
+    }
+}
+
+namespace Bitrix\Main\Composite {
+    class Helper extends \CHTMLPagesCache {
+    }
+}
+
+namespace Bitrix\Main\Entity\Field {
+    class IReadable extends \Bitrix\Main\ORM\Fields\IReadable {
+    }
+}
+
+namespace Bitrix\Main\Entity\Field {
+    class IStorable extends \Bitrix\Main\ORM\Fields\IStorable {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class BooleanField extends \Bitrix\Main\ORM\Fields\BooleanField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class DateField extends \Bitrix\Main\ORM\Fields\DateField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class DatetimeField extends \Bitrix\Main\ORM\Fields\DatetimeField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class EnumField extends \Bitrix\Main\ORM\Fields\EnumField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class ExpressionField extends \Bitrix\Main\ORM\Fields\ExpressionField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class IntegerField extends \Bitrix\Main\ORM\Fields\IntegerField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class FloatField extends \Bitrix\Main\ORM\Fields\FloatField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class StringField extends \Bitrix\Main\ORM\Fields\StringField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class TextField extends \Bitrix\Main\ORM\Fields\TextField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class CryptoField extends \Bitrix\Main\ORM\Fields\CryptoField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class ReferenceField extends \Bitrix\Main\ORM\Fields\Relations\Reference {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class ScalarField extends \Bitrix\Main\ORM\Fields\ScalarField {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class Field extends \Bitrix\Main\ORM\Fields\Field {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class FieldError extends \Bitrix\Main\ORM\Fields\FieldError {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class IValidator extends \Bitrix\Main\ORM\Fields\Validators\IValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class Base extends \Bitrix\Main\ORM\Fields\Validators\Validator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class Date extends \Bitrix\Main\ORM\Fields\Validators\DateValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class Enum extends \Bitrix\Main\ORM\Fields\Validators\EnumValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class Foreign extends \Bitrix\Main\ORM\Fields\Validators\ForeignValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class Length extends \Bitrix\Main\ORM\Fields\Validators\LengthValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class Range extends \Bitrix\Main\ORM\Fields\Validators\RangeValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class RegExp extends \Bitrix\Main\ORM\Fields\Validators\RegExpValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Validator {
+    class Unique extends \Bitrix\Main\ORM\Fields\Validators\UniqueValidator {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class INosqlPrimarySelector extends \Bitrix\Main\ORM\Query\INosqlPrimarySelector {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class NosqlPrimarySelector extends \Bitrix\Main\ORM\Query\NosqlPrimarySelector {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class Query extends \Bitrix\Main\ORM\Query\Query {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class QueryChain extends \Bitrix\Main\ORM\Query\Chain {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class QueryChainElement extends \Bitrix\Main\ORM\Query\ChainElement {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query\Filter\Expression {
+    class Base extends \Bitrix\Main\ORM\Query\Filter\Expressions\Expression {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query\Filter\Expression {
+    class Column extends \Bitrix\Main\ORM\Query\Filter\Expressions\ColumnExpression {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query\Filter\Expression {
+    class NullEx extends \Bitrix\Main\ORM\Query\Filter\Expressions\NullExpression {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class DataManager extends \Bitrix\Main\ORM\Data\DataManager {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class Result extends \Bitrix\Main\ORM\Data\Result {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class AddResult extends \Bitrix\Main\ORM\Data\AddResult {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class UpdateResult extends \Bitrix\Main\ORM\Data\UpdateResult {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class DeleteResult extends \Bitrix\Main\ORM\Data\DeleteResult {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query\Filter {
+    class Condition extends \Bitrix\Main\ORM\Query\Filter\Condition {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query\Filter {
+    class ConditionTree extends \Bitrix\Main\ORM\Query\Filter\ConditionTree {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query\Filter {
+    class Helper extends \Bitrix\Main\ORM\Query\Filter\Helper {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query\Filter {
+    class Operator extends \Bitrix\Main\ORM\Query\Filter\Operator {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query {
+    class Expression extends \Bitrix\Main\ORM\Query\Expression {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query {
+    class Join extends \Bitrix\Main\ORM\Query\Join {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query {
+    class Union extends \Bitrix\Main\ORM\Query\Union {
+    }
+}
+
+namespace Bitrix\Main\Entity\Query {
+    class UnionCondition extends \Bitrix\Main\ORM\Query\UnionCondition {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class Base extends \Bitrix\Main\ORM\Entity {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class EntityError extends \Bitrix\Main\ORM\EntityError {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class Event extends \Bitrix\Main\ORM\Event {
+    }
+}
+
+namespace Bitrix\Main\Entity {
+    class EventResult extends \Bitrix\Main\ORM\EventResult {
+    }
+}
+
+namespace Bitrix\Main\ORM {
+    class UField extends \Bitrix\Main\Entity\UField {
+    }
+}
+
+namespace Bitrix\Main\UserField {
+    class TypeBase extends \Bitrix\Main\UserField\Types\BaseType {
+    }
+}
+
+namespace Bitrix\Main\Data {
+    class AppCacheManifest extends \Bitrix\Main\Composite\AppCache {
+    }
+}
+
+namespace Bitrix\Main\Page {
+    class FrameBuffered extends \Bitrix\Main\Composite\BufferArea {
+    }
+}
+
+namespace Bitrix\Main\Page {
+    class FrameHelper extends \Bitrix\Main\Composite\BufferArea {
+    }
+}
+
+namespace Bitrix\Main\Page {
+    class Frame extends \Bitrix\Main\Composite\Engine {
+    }
+}
+
+class CHTMLPagesCache extends \Bitrix\Main\Composite\Helper {
+}
+
+namespace Bitrix\Main\Data {
+    class StaticHtmlCache extends \Bitrix\Main\Composite\Page {
+    }
+}
+
+class StaticHtmlCacheResponse extends \Bitrix\Main\Composite\AbstractResponse {
+}
+
+class StaticHtmlMemcachedResponse extends \Bitrix\Main\Composite\MemcachedResponse {
+}
+
+class StaticHtmlFileResponse extends \Bitrix\Main\Composite\FileResponse {
+}
+
+namespace Bitrix\Main\Page {
+    class FrameStatic extends \Bitrix\Main\Composite\StaticArea {
+    }
+}
+
+namespace Bitrix\Main\Data {
+    class StaticHtmlStorage extends \Bitrix\Main\Composite\Data\AbstractStorage {
+    }
+}
+
+namespace Bitrix\Main\Data {
+    class StaticCacheProvider extends \Bitrix\Main\Composite\Data\CacheProvider {
+    }
+}
+
+namespace Bitrix\Main\Data {
+    class StaticHtmlFileStorage extends \Bitrix\Main\Composite\Data\FileStorage {
+    }
+}
+
+namespace Bitrix\Main\Data {
+    class StaticHtmlMemcachedStorage extends \Bitrix\Main\Composite\Data\MemcachedStorage {
+    }
+}
+
+namespace Bitrix\Main\Composite {
+    class Engine extends \Bitrix\Main\Page\Frame {
+    }
+}
+
+namespace Bitrix\Pull {
+    class ChannelTable extends \Bitrix\Pull\Model\ChannelTable {
+    }
+}
+
+namespace Bitrix\Pull {
+    class PushTable extends \Bitrix\Pull\Model\PushTable {
+    }
+}
+
+namespace Bitrix\Sale\TradingPlatform {
+    class YandexMarket extends \Bitrix\Sale\TradingPlatform\YMarket\YandexMarket {
+    }
+}
+
+namespace Bitrix\Sale\PaySystem {
+    class ErrorLog extends \Bitrix\Sale\PaySystem\Logger {
+    }
+}
+
+namespace Bitrix\Sale {
+    class OrderTable extends \Bitrix\Sale\Internals\OrderTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class FuserTable extends \Bitrix\Sale\Internals\FuserTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class Product2ProductTable extends \Bitrix\Sale\Internals\Product2ProductTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class StoreProductTable extends \Bitrix\Sale\Internals\StoreProductTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class PersonTypeTable extends \Bitrix\Sale\Internals\PersonTypeTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class ProductTable extends \Bitrix\Sale\Internals\ProductTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class SectionTable extends \Bitrix\Sale\Internals\SectionTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class OrderProcessingTable extends \Bitrix\Sale\Internals\OrderProcessingTable {
+    }
+}
+
+namespace Bitrix\Sale {
+    class GoodsSectionTable extends \Bitrix\Sale\Internals\GoodsSectionTable {
+    }
+}
+
+namespace Bitrix\Sale\Delivery\Restrictions {
+    class PersonType extends \Bitrix\Sale\Delivery\Restrictions\ByPersonType {
+    }
+}
+
+namespace Bitrix\Sender {
+    class Connector extends \Bitrix\Sender\Connector\Base {
+    }
+}
+
+namespace Bitrix\Sender {
+    class ConnectorManager extends \Bitrix\Sender\Connector\Manager {
+    }
+}
+
+namespace Bitrix\Sender {
+    class ConnectorResult extends \Bitrix\Sender\Connector\Result {
+    }
+}
+
+namespace Bitrix\Sender {
+    class Trigger extends \Bitrix\Sender\Trigger\Base {
+    }
+}
+
+namespace Bitrix\Sender {
+    class TriggerManager extends \Bitrix\Sender\Trigger\Manager {
+    }
+}
+
+namespace Bitrix\Sender {
+    class TriggerSettings extends \Bitrix\Sender\Trigger\Settings {
+    }
+}
+
+namespace Bitrix\Sender {
+    class TriggerConnector extends \Bitrix\Sender\Trigger\TriggerConnector {
+    }
+}
+
+namespace Bitrix\Sender {
+    class TriggerConnectorClosed extends \Bitrix\Sender\Trigger\TriggerConnectorClosed {
+    }
+}
+
+namespace Bitrix\Tasks\DB {
+    class CommonHelper extends \Bitrix\Tasks\Internals\DataBase\Helper\Common {
+    }
+}
+
+namespace Bitrix\Tasks\DB {
+    class Helper extends \Bitrix\Tasks\Internals\DataBase\Helper {
+    }
+}
+
+namespace Bitrix\Tasks\DB {
+    class Tree extends \Bitrix\Tasks\Internals\DataBase\Tree {
+    }
+}
+
+namespace Bitrix\Tasks\DB {
+    class Mesh extends \Bitrix\Tasks\Internals\DataBase\Mesh {
+    }
+}
+
+namespace Bitrix\Tasks\DB\Tree {
+    class NodeNotFoundException extends \Bitrix\Tasks\Internals\DataBase\Tree\NodeNotFoundException {
+    }
+}
+
+namespace Bitrix\Tasks\DB\Tree {
+    class TargetNodeNotFoundException extends \Bitrix\Tasks\Internals\DataBase\Tree\TargetNodeNotFoundException {
+    }
+}
+
+namespace Bitrix\Tasks\DB\Tree {
+    class ParentNodeNotFoundException extends \Bitrix\Tasks\Internals\DataBase\Tree\ParentNodeNotFoundException {
+    }
+}
+
+namespace Bitrix\Tasks\DB\Tree {
+    class LinkExistsException extends \Bitrix\Tasks\Internals\DataBase\Tree\LinkExistsException {
+    }
+}
+
+namespace Bitrix\Tasks\DB\Tree {
+    class LinkNotExistException extends \Bitrix\Tasks\Internals\DataBase\Tree\LinkNotExistException {
+    }
+}
+
+namespace Bitrix\Tasks {
+    class TaskTable extends \Bitrix\Tasks\Integration\Report\Internals\TaskTable {
+    }
+}
+
+namespace Bitrix\Tasks {
+    class TagTable extends \Bitrix\Tasks\Internals\Task\TagTable {
+    }
+}
+
+namespace Bitrix\Tasks\Task {
+    class TagTable extends \Bitrix\Tasks\Internals\Task\TagTable {
+    }
+}
+
+namespace Bitrix\Tasks {
+    class MemberTable extends \Bitrix\Tasks\Internals\Task\MemberTable {
+    }
+}
+
+namespace Bitrix\Tasks {
+    class ElapsedTimeTable extends \Bitrix\Tasks\Internals\Task\ElapsedTimeTable {
+    }
+}
+
+namespace Bitrix\Tasks\Task {
+    class SortingTable extends \Bitrix\Tasks\Internals\Task\SortingTable {
+    }
+}
+
+namespace Bitrix\Tasks\Task {
+    class FavoriteTable extends \Bitrix\Tasks\Internals\Task\FavoriteTable {
+    }
+}
+
+namespace Bitrix\Tasks\Task {
+    class DependenceTable extends \Bitrix\Tasks\Internals\Task\ProjectDependenceTable {
+    }
+}
+
+namespace Bitrix\Tasks {
+    class TemplateTable extends \Bitrix\Tasks\Internals\Task\TemplateTable {
+    }
+}
+
+namespace Bitrix\Tasks\Template {
+    class DependencyTable extends \Bitrix\Tasks\Internals\Task\Template\DependenceTable {
+    }
+}
+
+namespace Bitrix\Tasks\Template {
+    class CheckListTable extends \Bitrix\Tasks\Internals\Task\Template\CheckListTable {
+    }
+}
+
+namespace Bitrix\Tasks\Template {
+    class CheckListItemTable extends \Bitrix\Tasks\Internals\Task\Template\CheckListTable {
+    }
+}
+
+namespace Bitrix\Tasks\Notification {
+    class ThrottleTable extends \Bitrix\Tasks\Internals\Notification\Task\ThrottleTable {
+    }
+}
+
+namespace Bitrix\Tasks\Util {
+    class UI extends \Bitrix\Tasks\UI {
+    }
+}
